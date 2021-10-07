3ea886e1fdb1ef5bf2ccef29e312c83f558ad3c3
Charmaine Lim <charmainelim@Charmaines-MacBook-Pro.local>
2021-10-07 16:09:41 +0800

    link local repo

---
+++ iOS BLE/iOS_BLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_BLEApp.swift
+//  iOS BLE
+//
+//  Created by Charmaine Lim on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_BLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
